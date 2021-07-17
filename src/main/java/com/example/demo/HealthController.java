67516e8c1f1e9c04d76203abe296caef1fd281f5
Anton Korenev <a.korenev2177@gmail.com>
2021-07-17 15:04:51 +0300

    homework completed

---
+++ src/main/java/com/example/demo/HealthController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping(value = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response health(){
+        return new Response();
+    }
+
+
+    private class Response {
+        private String status = "OK";
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+    }
+}
